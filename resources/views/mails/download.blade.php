e4c9cae53b9b9d243441a76fe624805df012bebe
vincent-tarrit <vincent@tarrit.com>
2025-09-17 23:44:24 +0200

    feat: remove downloading attachment

--- resources/views/mails/download.blade.php
+++ resources/views/mails/download.blade.php
@@ -1,3 +1,4 @@
+{{--
 <a type="button"
     href="{{ route('filament.' . Filament\Facades\Filament::getCurrentPanel()->getId() . '.mails.attachment.download', [
         'tenant' => Filament\Facades\Filament::getTenant(),
@@ -6,3 +7,4 @@
         'filename' => $getState()->filename,
     ]) }}"
     class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold cursor-pointer text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Download</a>
+--}}
